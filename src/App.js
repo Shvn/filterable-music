83708bfe81172385bf88ba08ecdf24206e0c26f9
Shvn <shvnry11@gmail.com>
2017-08-30 12:30:37 +0530

    Added static view

--- src/App.js
+++ src/App.js
@@ -2,20 +2,150 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class FilterableMusic extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <SearchBar />
+        <MusicTable musicList={this.props.musicList} />
+      </div>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search music"
+        />
+        <FilterOptions />
+      </form>
+    );
+  }
+}
+
+var TYPES = [
+  'Ghazal', 'Pop', 'Religious', 'Rock', 'Patriotic', 'Instrumental'
+];
+var MUSICIANS = [
+  'Rahat Fateh Ali Khan', 'Lata Mangeshkar', 'A R Rehman', 'Kishore Kumar', 'Sunidhi Chauhan', 'Bappi Lahiri'
+];
+
+class FilterOptions extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          <label>
+            Music Type
+            <MusicTypeFilter types={TYPES} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Musician
+            <MusicianFilter musicians={MUSICIANS} />
+          </label>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+class MusicTypeFilter extends Component {
+  render() {
+    var rows = [];
+    this.props.types.forEach(
+      (type) => {
+        rows.push(<MusicTypeInput type={type} />);
+      }
+    );
+    return (
+      <p>{rows}</p>
+    );
+  }
+}
+
+class MusicTypeInput extends Component {
+  render() {
+    return (
+      <span>
+        <input
+          type="checkbox"
+          name="musicType"
+        />
+        {' ' + this.props.type}
+      </span>
+    );
+  }
+}
+
+class MusicianFilter extends Component {
+  render() {
+    var rows = [];
+    this.props.musicians.forEach(
+      (musician) => {
+        rows.push(<MusicianInput musician={musician} />);
+      }
+    );
+    return (
+      <p>{rows}</p>
+    );
+  }
+}
+
+class MusicianInput extends Component {
+  render() {
+    return (
+      <span>
+        <input
+          type="checkbox"
+          name="musician"
+        />
+        {' ' + this.props.musician}
+      </span>
+    );
+  }
+}
+
+class MusicTable extends Component {
+  render() {
+    var rows = [];
+    this.props.musicList.forEach(
+      (music) => {
+        rows.push(<MusicRow music={music} />);
+      }
+    );
+    return (
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Duration</th>
+          <th>Musician</th>
+          <th>Mucic Type</th>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class MusicRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.music.name}</td>
+        <td>{this.props.music.duration}</td>
+        <td>{this.props.music.musician}</td>
+        <td>{this.props.music.type}</td>
+      </tr>
+    );
+  }
+}
+
+export default FilterableMusic;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FilterableMusic from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var MUSIC_LIST = [
+  {name: 'Mere Raske Qamar', duration: '5:01', musician: 'Rahat Fateh Ali Khan', type: 'Ghazal'},
+  {name: 'Tamma Tamma Again', duration: '4:44', musician: 'Lata Mangeshkar', type: 'Pop'},
+  {name: 'The Breakup Song', duration: '5:11', musician: 'A R Rehman', type: 'Religious'},
+  {name: 'Zaalima', duration: '3:51', musician: 'Kishore Kumar', type: 'Rock'},
+  {name: 'Ho Gya Hai Tujhko To', duration: '5:01', musician: 'Sunidhi Chauhan', type: 'Patriotic'},
+  {name: 'Lag Ja Gale Se Phir', duration: '4:36', musician: 'Bappi Lahiri', type: 'Instrumental'},
+  {name: 'Enna Sona', duration: '5:09', musician: 'A R Rehman', type: 'Ghazal'},
+  {name: 'Kala Chasma', duration: '3:22', musician: 'Kishore Kumar', type: 'Religious'},
+  {name: 'Piya More', duration: '4:32', musician: 'A R Rehman', type: 'Instrumental'},
+  {name: 'Galti Se Mistake', duration: '5:22', musician: 'Sunidhi Chauhan', type: 'Religious'},
+  {name: 'Khaana Khaake', duration: '6:02', musician: 'Rahat Fateh Ali Khan', type: 'Rock'},
+  {name: 'Chandralekha', duration: '4:00', musician: 'Sunidhi Chauhan', type: 'Rock'},
+  {name: 'Bakheda', duration: '4:59', musician: 'Bappi Lahiri', type: 'Pop'}
+];
+
+ReactDOM.render(<FilterableMusic musicList={MUSIC_LIST} />, document.getElementById('root'));
 registerServiceWorker();
